595262d658a63c32d6db6a461801066f623f9e1c
HuntingEagle0 <ss29aug2005@gmail.com>
2026-02-24 14:54:25 +0530

    Abstract Updates

--- main.dart
+++ main.dart
@@ -1,12 +1,37 @@
-class Student {
-  String? name;
-  int? age;
-  int? marks;
-  String? city;
-  Student(this.name, this.age, this.marks, this.city);
+// class Student {
+//   String? name;
+//   int? age;
+//   int? marks;
+//   String? city;
+//   Student(this.name, this.age, this.marks, this.city);
+// }
+
+// void main() {
+//   Student s1 = Student("Sachin", 20, 90, "Mumbai");
+//   print(s1.name);
+// }
+abstract class Shape {
+  void draw();
+}
+
+class Circle extends Shape {
+  @override
+  void draw() {
+    print("Drawing a circle");
+  }
+}
+
+class Square extends Shape {
+  @override
+  void draw() {
+    print("Drawing a square");
+  }
 }
 
 void main() {
-  Student s1 = Student("Sachin", 20, 90, "Mumbai");
-  print(s1.name);
+  Shape shape1 = Circle();
+  shape1.draw();
+
+  Shape shape2 = Square();
+  shape2.draw();
 }
